5e6baebce3442c6feeaa0ab404ca17644c2f589e
avfedorov1970 <78082708+avfedorov1970@users.noreply.github.com>
2021-02-02 17:25:49 +0300

    Add files via upload

---
+++ Module_0/Module_0_Andrey_Fedorov.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+     count = 1
+    predict = np.random.randint(1,101)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += 1
+        elif number < predict: 
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
